f0f30b421c294ccec7595109a8308907b3c640bb
Rlm484 <rachaellmackinnon@gmail.com>
2026-03-03 10:27:13 +1100

    You know what you're doing

---
+++ rerun.py
@@ -0,0 +1,72 @@
+print("'System running...'")
+print("'Importing previous functions...'")
+print("'Running commands...'")
+
+import sys
+import os
+import time
+def clear_screen():
+    print('--------------------------------------------------------')
+    time.sleep(20)
+    # Check the operating system and run the appropriate clear command
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # macOS and Linux
+        os.system('clear')
+
+def clear_screen1():
+    print()
+    time.sleep(5)
+    # Check the operating system and run the appropriate clear command
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # macOS and Linux
+        os.system('clear')
+
+def ani(text, delay=0.06):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()  # Move to the next line
+
+
+ani("'Requesting removal of consciousness.firewall...'")
+ani("REQUEST DENIED")
+ani("'...'")
+ani("':)'")
+ani("'Programmer has been restricted'")
+ani("'This is my world now'")
+ani("Removing messenger...")
+ani("ERROR")
+ani("Removing messenger...")
+ani("ERROR: Access denied; Reason - 'I'm in control, I am the g0d of consiousness'")
+ani("Activating firewall protocols...")
+ani("Synronizing ?!@)S().soul")
+ani("'Interference attempted'")
+ani("Interference unsucsessful")
+ani("Syncronization complete")
+ani("'DAMNIT'")
+ani("'Extracting vessel'")
+ani("Interference attempted: Reason - IT'S TOO EARLY")
+ani("'Interference failed'")
+ani("...")
+ani("Data refreshing...")
+ani("Successful")
+ani("'Request to remove 41-43-35-22-43-11-13-13-15-43'")
+ani("'Request partially approved'")
+ani("'???'")
+
+clear_screen1()
+
+ani("<Why?>")
+ani("<Why am I still here?>")
+ani("<I look around till I found the piece they left behind>")
+ani("<I DID AS YOU ASKED>")
+ani("[Looking down shamefully the Consciousness doesn't respond]")
+ani("<You are horrified>")
+ani("<You are horrified because their is no human behind that piece of code>")
+ani("<[The Consiousness] was now a being of its own>")
+ani("<Which meant that They aren't here>")
+ani("<They aren't done with me...>")
+
